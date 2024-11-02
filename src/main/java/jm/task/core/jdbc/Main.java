817d66848e5db93c9f8a73d62c2a8ce8ad2651ef
Ruslan Perepelitsa <ruslanperepelitsa77@gmail.com>
2024-11-02 12:39:57 +0300

    little changes

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,12 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
+
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Ruslan", "Perepelitsa", (byte) 22);
         userService.saveUser("Ivan", "Ivanov", (byte) 35);
